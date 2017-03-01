fb3ad115d8d85597a83b8da2f11fb6362b5195bd
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2017-03-01 21:36:45 +0100

    Added a team name field to team model

--- scoreboard/models.py
+++ scoreboard/models.py
@@ -4,6 +4,10 @@ from django.db import models
 
 # Create your models here.
 class Team(models.Model):
+    name = models.CharField(max_length=10000)
     members = models.ManyToManyField('auth.User', related_name='members', blank=True)
     score = models.IntegerField(default=0)
     beers = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
