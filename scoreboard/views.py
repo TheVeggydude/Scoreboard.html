4fe326e7ec7629546516be6b6a8b3abeab876474
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2017-03-01 21:37:47 +0100

    Updated the view of index page to show table of teams

--- scoreboard/views.py
+++ scoreboard/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Team
+import django_tables2 as tables
+
+
+class TeamTable(tables.Table):
+    class Meta:
+        model = Team
+        fields = ('name', 'score', 'beers')
+        attrs = {"class": "table"}
+        empty_text = "No teams available"
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    teams = Team.objects.all()
+    teams_table = TeamTable(teams, prefix='teams-')
+    tables.config.RequestConfig(request).configure(teams_table)
+
+    return render(request, 'scoreboard/index.html', {'teams_table': teams_table})
