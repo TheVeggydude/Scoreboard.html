96de3df521aa1a97a16aa6d27d4a4fa8c1ffe370
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2017-03-01 20:02:52 +0100

    Created Team model

--- scoreboard/models.py
+++ scoreboard/models.py
@@ -1,5 +1,9 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
+
 # Create your models here.
+class Team(models.Model):
+    members = models.ManyToManyField('auth.User', related_name='members', blank=True)
+    score = models.IntegerField(default=0)
+    beers = models.IntegerField(default=0)
