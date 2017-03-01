13dc7efb4da5ae473ce4e21a94a119ff4eb186e7
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2017-03-01 21:36:30 +0100

    Added django_tables2 to the installed apps

--- cenc/settings.py
+++ cenc/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_tables2',
     'scoreboard.apps.ScoreboardConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -63,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.request',
             ],
         },
     },
