bec8f06f07c13220249b8e41161f14d7f7372f8b
Jeroen Langhorst <jeroenlanghorst@gmail.com>
2017-03-07 19:20:47 +0100

    Fixed urls, so everything points to everything

--- cenc/urls.py
+++ cenc/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^scoreboard/', include('scoreboard.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^', include('scoreboard.urls')),
 ]
--- scoreboard/urls.py
+++ scoreboard/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^exercises/', views.exercises, name='exercises'),
+
 ]
